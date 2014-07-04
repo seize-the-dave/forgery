268585f53ab46108da6d54bdb5b183de81fde3b3
Dave Grant <david@grant.org.uk>
2014-07-04 17:05:19 +0100

    Update Forgery.java

--- src/main/java/uk/co/adaptivelogic/forgery/Forgery.java
+++ src/main/java/uk/co/adaptivelogic/forgery/Forgery.java
@@ -1,7 +1,7 @@
 package uk.co.adaptivelogic.forgery;
 
 public class Forgery {
-  public static <T> T forge(Class<T> type) {
+  public static <T> T forge(Class<T> type) throws InstantiationException, IllegalAccessException {
     return type.newInstance();
   }
 }
