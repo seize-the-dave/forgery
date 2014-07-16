ab4c17e624dfa53bf6c9ca2a680fcf63c8ec9881
David Grant <david@grant.org.uk>
2014-07-16 09:10:09 +0100

    Convert tabs to spaces

--- src/test/java/uk/co/adaptivelogic/forgery/ForgerRegistryTest.java
+++ src/test/java/uk/co/adaptivelogic/forgery/ForgerRegistryTest.java
@@ -1,7 +1,6 @@
 package uk.co.adaptivelogic.forgery;
 
 import com.google.common.base.Optional;
-import org.junit.Before;
 import org.junit.Test;
 import uk.co.adaptivelogic.forgery.forger.RandomLongForger;
 
@@ -9,52 +8,52 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class ForgerRegistryTest {
-	@Test
-	public void shouldFindTypeForger() {
-		// Given
-		Forger<Long> expected = new RandomLongForger();
+    @Test
+    public void shouldFindTypeForger() {
+        // Given
+        Forger<Long> expected = new RandomLongForger();
         ForgerRegistry registry = new InMemoryForgerRegistry(expected);
 
-		// When
-		Optional<Forger<Long>> actual = registry.lookup(Long.class);
+        // When
+        Optional<Forger<Long>> actual = registry.lookup(Long.class);
 
-		// Then
-		assertThat(actual.get(), is(expected));
-	}
+        // Then
+        assertThat(actual.get(), is(expected));
+    }
 
-	@Test
-	public void shouldNotFindTypeForgerForInvalidType() {
-		// When
+    @Test
+    public void shouldNotFindTypeForgerForInvalidType() {
+        // When
         ForgerRegistry registry = new InMemoryForgerRegistry();
-		Optional<Forger<String>> actual = registry.lookup(String.class);
+        Optional<Forger<String>> actual = registry.lookup(String.class);
 
-		// Then
-		assertThat(actual.isPresent(), is(false));
-	}
+        // Then
+        assertThat(actual.isPresent(), is(false));
+    }
 
-	@Test
-	public void shouldFindPropertyForgerForProperty() {
-		// Given
-		Forger<String> expected = new FirstNameStringForger();
+    @Test
+    public void shouldFindPropertyForgerForProperty() {
+        // Given
+        Forger<String> expected = new FirstNameStringForger();
         ForgerRegistry registry = new InMemoryForgerRegistry(expected);
 
-		// When
-		Optional<Forger<String>> actual = registry.lookup(String.class, "firstName");
+        // When
+        Optional<Forger<String>> actual = registry.lookup(String.class, "firstName");
 
-		// Then
-		assertThat(actual.get(), is(expected));
-	}
+        // Then
+        assertThat(actual.get(), is(expected));
+    }
 
-	@Test
-	public void shouldNotFindPropertyForgerForInvalidProperty() {
-		// Given
-		Forger<String> expected = new FirstNameStringForger();
+    @Test
+    public void shouldNotFindPropertyForgerForInvalidProperty() {
+        // Given
+        Forger<String> expected = new FirstNameStringForger();
         ForgerRegistry registry = new InMemoryForgerRegistry(expected);
 
-		// When
-		Optional<Forger<String>> actual = registry.lookup(String.class, "lastName");
+        // When
+        Optional<Forger<String>> actual = registry.lookup(String.class, "lastName");
 
-		// Then
-		assertThat(actual.isPresent(), is(false));
-	}
+        // Then
+        assertThat(actual.isPresent(), is(false));
+    }
 }
